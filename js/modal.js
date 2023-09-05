449d8bf37de74e1add5510b8a3aca9377ffa7014
Belka-S <belka.srg@gmail.com>
2023-09-05 21:45:48 +0200

    fix svg

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-=======
 const ref = {
   modalEl: document.querySelector('div[data-modal]'),
   openBtnEl: document.querySelector('button[data-modal-open]'),
@@ -25,4 +9,3 @@ const onClick = e => ref.modalEl.classList.toggle('is-hidden');
 
 ref.openBtnEl.addEventListener('click', onClick);
 ref.closeBtnEl.addEventListener('click', onClick);
->>>>>>> ff841ac4afd56a437eaf1b057636f2d5bfc30c89
